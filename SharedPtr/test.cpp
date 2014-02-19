8daf9e9075314a087ac45e27558eea83b5469184
anderschen <chenhf2010@qq.com>
2014-02-19 21:09:01 +0800

    commit

--- SharedPtr/test.cpp
+++ SharedPtr/test.cpp
@@ -3,10 +3,29 @@
 
 class session
 {
+public:
+	~session()
+	{
+		std::cout << "~session\n";
+	}
 
+	void print()
+	{
+	}
 };
 
 void main()
 {
+	std::shared_ptr<session> ses = std::make_shared<session>();
+	std::cout << "ses count = " << ses.use_count() << "\n";
 
+	auto two = ses;
+	std::cout << "ses count = " << ses.use_count() << "\n";
+	std::cout << "two count = " << two.use_count() << "\n";
+
+	auto& three = ses;
+	std::cout << "three count = " << three.use_count() << "\n";
+
+	ses.reset();
+	std::cout << "ses count = " << ses.use_count() << "\n";
 }
